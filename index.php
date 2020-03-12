175731ad97c34a1553bdc14d1b13a52f6fa04c3d
Vitaliy <kudrvet@gmail.com>
2020-03-12 22:27:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use My\Project\Runner;
+
+print_r(Runner\run());
---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+namespace My\Project\Runner;
+
+use Illuminate\Support\Collection;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
